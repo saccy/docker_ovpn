2f70d53c595a618eab4933b53110f3e673236137
andrew hudson <a.j.hudson@hotmail.com>
2020-10-21 18:06:52 +1100

    removed client env var

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-client='saccy'
-
 docker run \
     --rm \
     -d \
     -p 1194:1194/udp \
     -e EASYRSA_BATCH=1 \
     -e PUBLIC_DNS="$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)" \
-    -e CLIENT=${client} \
     -e DEBUG=1 \
     -v /tmp/docker_out/:/docker_out/:Z \
     --cap-add=NET_ADMIN \
